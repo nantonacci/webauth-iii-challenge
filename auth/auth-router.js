43b533070ac91ac94be16163346e3ebb3a974140
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-12-10 21:45:06 -0800

    MVP

---
+++ auth/auth-router.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets.js");
+
+const Users = require("../users/users-model.js");
+
+router.post("/register", (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      const token = getToken(saved);
+      res.status(201).json({ created_user: saved, token: token });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = genToken(user);
+        res.status(200).json({ username: user.username, token: token });
+      } else {
+        res.status(401).json({ message: "You shall not pass!" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+function genToken(user) {
+  const payload = {
+    userid: user.id,
+    username: user.username,
+    roles: ["Student"]
+  };
+  const options = { expiresIn: "1h" };
+  const token = jwt.sign(payload, secrets.jwtSecret, options);
+
+  return token;
+}
+
+module.exports = router;
